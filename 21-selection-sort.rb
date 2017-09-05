b2b6bea2d7370129e937f89ad57d104935f2078f
韦月炎 <weiyueyan@MBP.local>
2017-09-05 16:06:46 +0800

    21-selection-sort.rb

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,20 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  array = []
+  while 0 < arr.size
+    a = arr[0]
+    b = 0
+    arr.each_with_index do |i, j|
+      if i < a
+        a = i
+        b = j
+      end
+    end
+    arr.delete_at(b)
+    array << a
+  end
+  return array
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
