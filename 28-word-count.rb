96bbd2c57c2608c4f1fc978824b27e358bb86f0d
韦月炎 <weiyueyan@MBP.local>
2017-09-05 20:56:45 +0800

    18-square.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,21 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
 doc = File.read("wordcount.txt")
+arr = doc.gsub(/[.,]/ ,"").split(" ")
 
+def count(arr)
+  array = {}
+  arr.each do |i|
+    if array[i]
+      array[i] += 1
+    else
+      array[i] = 1
+    end
+  end
+  return array
+end
 # ...
+
+answer = count(arr)
+
+puts answer
